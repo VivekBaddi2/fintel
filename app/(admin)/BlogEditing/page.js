65645d27ec33b12c2bd8fabe19db1fe140174851
Vivek <vivekbaddi2@gmail.com>
2025-10-12 12:18:08 +0530

    updated import path

--- app/(admin)/BlogEditing/page.js
+++ app/(admin)/BlogEditing/page.js
@@ -4,7 +4,7 @@ import dynamic from "next/dynamic";
 
 // Dynamically import the client component to prevent SSR
 const BlogEditorClient = dynamic(
-  () => import("../../../admin-client-pages/BlogEditing/BlogEditorClient"),
+  () => import("../../admin-client-pages/BlogEditing/BlogEditorClient"),
   { ssr: false } // <- prevents server-side rendering
 );
 
