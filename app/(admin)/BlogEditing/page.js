213ab2d71d0c7ad9e49875c2523caa440f44f0e4
Vivek <vivekbaddi2@gmail.com>
2025-10-12 10:28:38 +0530

    fixed pre rendering issue using react Suspense

--- app/(admin)/BlogEditing/page.js
+++ app/(admin)/BlogEditing/page.js
@@ -1,6 +1,6 @@
 "use client";
 import { useSearchParams } from "next/navigation";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Suspense } from "react";
 import { ToastContainer, toast, Slide } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -84,71 +84,73 @@ export default function AdminDashboard() {
     return <p className="text-center mt-24 text-gray-500">Loading...</p>;
 
   return (
-    <div className="max-w-7xl mx-auto p-6 space-y-8">
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop
-        closeOnClick
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        transition={Slide}
-      />
+    <Suspense>
+      <div className="max-w-7xl mx-auto p-6 space-y-8">
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          transition={Slide}
+        />
 
-      <h1 className="text-3xl font-bold text-gray-800">Blog Editor</h1>
+        <h1 className="text-3xl font-bold text-gray-800">Blog Editor</h1>
 
-      {/* Add/Edit Blog Form */}
-      <div className="bg-transparent rounded-lg ">
-        <h2 className="text-xl font-semibold mb-4">
-          {blogId ? "Edit Blog" : "Add New Blog"}
-        </h2>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <label htmlFor="blogTitleInput" className="font-medium">
-            Blog Title
-          </label>
-          <input
-            type="text"
-            id="blogTitleInput"
-            placeholder="Title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            className="w-full p-3 mt-2 border rounded-lg focus:outline-none bg-white focus:ring-2 focus:ring-gray-300"
-          />
+        {/* Add/Edit Blog Form */}
+        <div className="bg-transparent rounded-lg ">
+          <h2 className="text-xl font-semibold mb-4">
+            {blogId ? "Edit Blog" : "Add New Blog"}
+          </h2>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <label htmlFor="blogTitleInput" className="font-medium">
+              Blog Title
+            </label>
+            <input
+              type="text"
+              id="blogTitleInput"
+              placeholder="Title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className="w-full p-3 mt-2 border rounded-lg focus:outline-none bg-white focus:ring-2 focus:ring-gray-300"
+            />
 
-          <label htmlFor="blogDescriptionInput" className="font-medium">
-            Blog Description
-          </label>
-          <textarea
-            id="blogDescriptionInput"
-            placeholder="Description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            className="w-full p-3 mt-2 border rounded-lg focus:outline-none bg-white focus:ring-2 focus:ring-gray-300"
-          />
+            <label htmlFor="blogDescriptionInput" className="font-medium">
+              Blog Description
+            </label>
+            <textarea
+              id="blogDescriptionInput"
+              placeholder="Description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              className="w-full p-3 mt-2 border rounded-lg focus:outline-none bg-white focus:ring-2 focus:ring-gray-300"
+            />
 
-          <label htmlFor="blogContentInput" className="font-medium">
-            Blog Content
-          </label>
-          <textarea
-            id="blogContentInput"
-            placeholder="Content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-            className="w-full p-3 mt-2 border rounded-lg h-64 focus:outline-none bg-white focus:ring-2 focus:ring-gray-300"
-          />
+            <label htmlFor="blogContentInput" className="font-medium">
+              Blog Content
+            </label>
+            <textarea
+              id="blogContentInput"
+              placeholder="Content"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              className="w-full p-3 mt-2 border rounded-lg h-64 focus:outline-none bg-white focus:ring-2 focus:ring-gray-300"
+            />
 
-          <div className="flex items-center space-x-4">
-            <button
-              type="submit"
-              className="active:scale-95 bg-gray-900 text-white font-medium px-6 py-2 rounded-lg hover:bg-gray-800 transition cursor-pointer"
-            >
-              {blogId ? "Update Blog" : "Add Blog"}
-            </button>
-          </div>
-        </form>
+            <div className="flex items-center space-x-4">
+              <button
+                type="submit"
+                className="active:scale-95 bg-gray-900 text-white font-medium px-6 py-2 rounded-lg hover:bg-gray-800 transition cursor-pointer"
+              >
+                {blogId ? "Update Blog" : "Add Blog"}
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
